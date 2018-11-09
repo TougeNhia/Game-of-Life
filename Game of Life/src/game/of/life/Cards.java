ae2f76c62383975ffe09515fd130efc3e1d7e5e2
joshuaUm <44787024+joshuaUm@users.noreply.github.com>
2018-11-09 12:13:33 -0800

    cards

--- Game of Life/src/game/of/life/Cards.java
+++ Game of Life/src/game/of/life/Cards.java
@@ -2,11 +2,18 @@ package game.of.life;
 import java.util.ArrayList;
 public class Cards {
     private String description;
+    private int salary;
+    private int raise;
+    private String name;
     private static ArrayList<Cards> cards = new ArrayList();
     
     
-    Cards(String _description){
+    Cards(String _description, int _salary, int _raise, String _name){
+        name = _name;
         description = _description;
+        salary = _salary;
+        raise = _raise;
+        
     }
     
     
