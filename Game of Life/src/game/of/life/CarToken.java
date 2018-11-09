cd45e1246a822a3cef14974b967267ed383cd926
joshuaUm <44787024+joshuaUm@users.noreply.github.com>
2018-11-09 12:25:05 -0800

    everything lmao

--- Game of Life/src/game/of/life/CarToken.java
+++ Game of Life/src/game/of/life/CarToken.java
@@ -4,11 +4,16 @@ import java.awt.*;
 
 public class CarToken {
     private Color color;
+    private Player slots[] = new Player[Player.getNumPlayers()];
+    private Player car;
     CarToken(Color _color){
         color = _color;
+        for(int i=0;i<slots.length;i++)
+            slots[i] = null;
     }
     public void draw(Graphics2D g,int row,int column,int xdelta,int ydelta) {
         g.setColor(color); 
+        g.fillRect(Window.getX(column*xdelta),Window.getY(row*ydelta),xdelta,ydelta); 
         
     }
 }
--- Game of Life/src/game/of/life/Player.java
+++ Game of Life/src/game/of/life/Player.java
@@ -1,4 +1,25 @@
 package game.of.life;
+import java.awt.*;
 public class Player {
+    final private static int numPlayers = 4;
+    private Image car; 
+    private int moves;
+    private static Player currPlayer;
+    private static Player players[] = new Player[numPlayers];    
     
+    Player(Image _car){
+     moves = 0;
+     car = _car;
+    }
+    
+    public static int getNumPlayers(){
+        return(numPlayers);
+    }
+    
+    public void move(){
+        if(currPlayer == this){
+            
+            moves--;
+        }
+    }
 }
