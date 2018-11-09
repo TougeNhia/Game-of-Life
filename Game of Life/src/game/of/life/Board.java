ce36cd956fc08a755bc3a60592dc61c6e45a3381
joshuaUm <44787024+joshuaUm@users.noreply.github.com>
2018-11-09 12:09:05 -0800

    board

--- Game of Life/src/game/of/life/Board.java
+++ Game of Life/src/game/of/life/Board.java
@@ -3,8 +3,8 @@ package game.of.life;
 import java.awt.*;
 
 public class Board {
-    private final static int NUM_ROWS = 10;
-    private final static int NUM_COLUMNS = 15;      
+    private final static int NUM_ROWS = 14;
+    private final static int NUM_COLUMNS = 21;      
     private static CarToken board[][] = new CarToken[NUM_ROWS][NUM_COLUMNS];
     
         public static void AddTokenPixel(int xpixel,int ypixel) {
@@ -36,6 +36,8 @@ public class Board {
     }
     
         public static void Draw(Graphics2D g) {
+            Image image =  Toolkit.getDefaultToolkit().getImage("./board.png");    
+            Drawing.drawImage(image, Window.getX(Window.getWidth2()/2), Window.getY(Window.getHeight2()/2), 0.0, 1.0, 1.0);
 //Calculate the width and height of each board square.
         int ydelta = Window.getHeight2()/NUM_ROWS;
         int xdelta = Window.getWidth2()/NUM_COLUMNS;
