1e79eeeaafec06ee29c78807150f0e58b7cc1f02
joshuaUm <44787024+joshuaUm@users.noreply.github.com>
2018-11-16 12:30:31 -0800

    working on my own branch

--- Game of Life/src/game/of/life/Button.java
+++ Game of Life/src/game/of/life/Button.java
@@ -1,37 +1,4 @@
-<<<<<<< current
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package game.of.life;
 
-import java.awt.*;
-
-public class Button extends Page {
-    private boolean isActive;
-    Button(String name, Tab _tab, int _xsize, int _ysize, int _xpos, int _ypos){
-    super(name, _tab);
-    xsize = _xsize;
-    ysize = _ysize;
-    xpos = _xpos;
-    ypos = _ypos;
-    }
-    public boolean getActive(){
-        return isActive;
-    }
-    public void pressed(){
-        if(isActive){
-            
-        }
-    }
-    public void drawHitBox(Graphics2D g){
-        g.setColor(Color.red);
-        g.drawRect(xpos, ypos, xsize, ysize);
-    }
-    
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -43,10 +10,10 @@ import java.awt.*;
 
 public class Button extends Page {
     private Image image;
-    private int xpos  = Window.getX(xdelta * 14)- (Drawing.getImageWidth(image)/2);
-    private int ypos = Window.getY(ydelta * 7)- (Drawing.getImageHeight(image)/2);
-    private int xsize = Window.getX(xdelta * 14) + (Drawing.getImageWidth(image)/2);
-    private int ysize = Window.getY(ydelta * 7) + (Drawing.getImageHeight(image)/2);
+    private int xpos;
+    private int ypos;
+    private int xsize;
+    private int ysize;
     private boolean isActive;
     Button(String name, Tab _tab, int _xsize, int _ysize, int _xpos, int _ypos){
     super(name, _tab);
@@ -69,4 +36,3 @@ public class Button extends Page {
     }
     
 }
->>>>>>> before discard
